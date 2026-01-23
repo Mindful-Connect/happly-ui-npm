5ba26a5b9269e79022a3a4a2f8949ac51efa47b8
Dinesh Pushparajah <dinesh@happly.ai>
2026-01-22 23:30:03 -0500

    feat(docs): add custom domain ui.happly.cloud

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -6,7 +6,6 @@ import withSearch from './src/markdoc/search.mjs'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
   output: 'export',
-  basePath: '/happly-ui-npm',
   images: {
     unoptimized: true,
   },
