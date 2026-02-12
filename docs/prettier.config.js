2f1396c80a45b2df444c8f15f221e73661fea1e3
Yevgeniy Akimenko <yevgeniy@happly.ai>
2026-02-12 07:53:18 -0800

    chore: Update Prettier configuration to enforce semicolons and add new formatting options.

--- docs/prettier.config.js
+++ docs/prettier.config.js
@@ -1,7 +1,10 @@
 /** @type {import('prettier').Options} */
 module.exports = {
   singleQuote: true,
-  semi: false,
+  semi: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  jsxSingleQuote: true,
   plugins: ['prettier-plugin-tailwindcss'],
   tailwindStylesheet: './src/styles/tailwind.css',
 }
